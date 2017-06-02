512303470d608aece610e56724fa8817af97d428
Steve Price <spprice@nola.gov>
2017-06-02 17:37:13 -0500

    move campaignasseturl config to external file

--- Kentico.EntityFramework/Configuration/Analytics/CampaignAssetUrlConfiguration.cs
+++ Kentico.EntityFramework/Configuration/Analytics/CampaignAssetUrlConfiguration.cs
@@ -11,7 +11,39 @@ namespace Kentico.EntityFramework.Configuration.Analytics
     {
         public static void Configure(ModelBuilder modelBuilder)
         {
-            throw new NotImplementedException();
+            modelBuilder.Entity<CampaignAssetUrl>(entity =>
+            {
+                entity.HasKey(e => e.CampaignAssetUrlId)
+                    .HasName("PK_Analytics_CampaignAssetUrl");
+
+                entity.ToTable("Analytics_CampaignAssetUrl");
+
+                entity.HasIndex(e => e.CampaignAssetUrlCampaignAssetId)
+                    .HasName("IX_Analytics_CampaignAssetUrl_CampaignAssetUrlCampaignAssetID");
+
+                entity.Property(e => e.CampaignAssetUrlId).HasColumnName("CampaignAssetUrlID");
+
+                entity.Property(e => e.CampaignAssetUrlCampaignAssetId)
+                    .HasColumnName("CampaignAssetUrlCampaignAssetID")
+                    .HasDefaultValueSql("0");
+
+                entity.Property(e => e.CampaignAssetUrlGuid).HasDefaultValueSql("'00000000-0000-0000-0000-000000000000'");
+
+                entity.Property(e => e.CampaignAssetUrlPageTitle)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .HasDefaultValueSql("N''");
+
+                entity.Property(e => e.CampaignAssetUrlTarget)
+                    .IsRequired()
+                    .HasDefaultValueSql("N''");
+
+                entity.HasOne(d => d.CampaignAssetUrlCampaignAsset)
+                    .WithMany(p => p.AnalyticsCampaignAssetUrl)
+                    .HasForeignKey(d => d.CampaignAssetUrlCampaignAssetId)
+                    .OnDelete(DeleteBehavior.Restrict)
+                    .HasConstraintName("FK_Analytics_CampaignAssetUrl_CampaignAssetUrlCampaignAssetID_Analytics_CampaignAsset");
+            });
         }
     }
 }
--- Kentico.EntityFramework/KenticoContext.cs
+++ Kentico.EntityFramework/KenticoContext.cs
@@ -359,40 +359,6 @@ namespace Kentico.EntityFramework
 
             CampaignAssetUrlConfiguration.Configure(modelBuilder);
 
-            modelBuilder.Entity<CampaignAssetUrl>(entity =>
-            {
-                entity.HasKey(e => e.CampaignAssetUrlId)
-                    .HasName("PK_Analytics_CampaignAssetUrl");
-
-                entity.ToTable("Analytics_CampaignAssetUrl");
-
-                entity.HasIndex(e => e.CampaignAssetUrlCampaignAssetId)
-                    .HasName("IX_Analytics_CampaignAssetUrl_CampaignAssetUrlCampaignAssetID");
-
-                entity.Property(e => e.CampaignAssetUrlId).HasColumnName("CampaignAssetUrlID");
-
-                entity.Property(e => e.CampaignAssetUrlCampaignAssetId)
-                    .HasColumnName("CampaignAssetUrlCampaignAssetID")
-                    .HasDefaultValueSql("0");
-
-                entity.Property(e => e.CampaignAssetUrlGuid).HasDefaultValueSql("'00000000-0000-0000-0000-000000000000'");
-
-                entity.Property(e => e.CampaignAssetUrlPageTitle)
-                    .IsRequired()
-                    .HasMaxLength(200)
-                    .HasDefaultValueSql("N''");
-
-                entity.Property(e => e.CampaignAssetUrlTarget)
-                    .IsRequired()
-                    .HasDefaultValueSql("N''");
-
-                entity.HasOne(d => d.CampaignAssetUrlCampaignAsset)
-                    .WithMany(p => p.AnalyticsCampaignAssetUrl)
-                    .HasForeignKey(d => d.CampaignAssetUrlCampaignAssetId)
-                    .OnDelete(DeleteBehavior.Restrict)
-                    .HasConstraintName("FK_Analytics_CampaignAssetUrl_CampaignAssetUrlCampaignAssetID_Analytics_CampaignAsset");
-            });
-
             modelBuilder.Entity<CampaignConversion>(entity =>
             {
                 entity.HasKey(e => e.CampaignConversionId)
